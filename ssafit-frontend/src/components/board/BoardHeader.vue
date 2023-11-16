a451c1b4cec5f687f2cb9c81c3c1e57b1f898ca0
jsong98 <jswsn1526@gmail.com>
2023-11-16 17:52:31 +0900

    feat: videoList

--- ssafit-frontend/src/components/board/BoardHeader.vue
+++ ssafit-frontend/src/components/board/BoardHeader.vue
@@ -1,10 +1,7 @@
 <template>
     <div class="container text-center">
-        <!-- boardList에 있는 모든 board들을 순회한다.-->
         <RouterLink :to="{ name: 'free' }">자유게시판</RouterLink> | 
         <RouterLink :to="{ name: 'announce' }">공지사항</RouterLink> | 
-        <!-- <RouterLink to="/board/announce" @click="announce">공지사항</RouterLink> |
-        <RouterLink to="/board/free" @click="free">자유게시판</RouterLink> -->
     </div>
 </template>
 
@@ -13,20 +10,9 @@ import { ref,computed,onMounted } from 'vue';
 import { useRouter } from 'vue-router';
 import { useBoardStore } from '@/stores/board.js'
 
-const boardStore = useBoardStore()
-
-
-// boardId를 넣으면 boardId에 해당하는(게시판에 속한) articleList로 갱신한다.
-const getArticles = (boardId) => {
-    boardStore.getArticleList(boardId)
-    boardStore.getBoard(boardId)
-}
-
-// useBoardStore에 있는 boardList를 caching하기 위한 코드
-const boardList = computed(() => JSON.parse(localStorage.getItem("boardList")))
 
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
--- ssafit-frontend/src/views/VideoView.vue
+++ ssafit-frontend/src/views/VideoView.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
-    <h2 class="text-center"><i class="bi bi-youtube" style="color: red;"></i> YoutubeView</h2>
+    <h2 class="text-center"><i class="bi bi-youtube" style="color: red;"></i>유튜브 영상</h2>
     <hr>
+    <VideoHeader/>
+    <RouterView/>
   </div>
 </template>
 
 <script setup>
+import VideoHeader from '../components/video/VideoHeader.vue';
 </script>
 
-<style scoped></style>
+<style scoped>
+</style>
